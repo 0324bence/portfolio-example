3f83ac994e109646aec30409c5ff3d7167dc81b7
0324bence <d.bence0324@gmail.com>
2023-10-26 13:14:43 +0200

    Create header

---
+++ src/components/Header.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { base } from "$app/paths";
+</script>
+
+<div id="header">
+    <h1>Fényképész úr</h1>
+    <div id="links">
+        <a href="{base}/">Képvásárlás</a>
+        <a href="{base}/">Blog</a>
+        <a href="{base}/">Kosár</a>
+    </div>
+</div>
+
+<style lang="scss">
+    //testing
+    :global(body) {
+        background-color: black;
+    }
+
+    #header {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        color: white;
+
+        h1 {
+            font-family: "special elite", sans-serif;
+            font-size: 4rem;
+        }
+        a {
+            color: inherit;
+            text-decoration: none;
+        }
+
+        #links {
+            display: flex;
+            align-items: center;
+            width: 20%;
+            justify-content: space-around;
+            font-family: "rubik", sans-serif;
+            font-size: 1.5rem;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Header from "../components/Header.svelte";
+</script>
+
+<div id="main-body">
+    <Header />
+    <div id="content"><h1>asd</h1></div>
+</div>
+
+<style lang="scss">
+    #main-body {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+    }
+</style>
