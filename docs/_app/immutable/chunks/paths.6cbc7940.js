4be59180543c63de1b532ff9c45a2f3fe01283cf
0324bence <bence20050324@gmail.com>
2023-11-05 14:45:27 +0100

    Upload first build

---
+++ docs/_app/immutable/chunks/paths.6cbc7940.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_172p0ho)==null?void 0:s.base)??"/portfolio-example";var e;const o=((e=globalThis.__sveltekit_172p0ho)==null?void 0:e.assets)??a;export{o as a,a as b};
