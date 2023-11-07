e389acbe6ad7c47ddd3f438a865d9fc598f36648
mikobalint <92861159+mikobalint@users.noreply.github.com>
2023-11-07 21:59:28 +0100

    The getColWidth function

--- src/routes/Gallery/+page.svelte
+++ src/routes/Gallery/+page.svelte
@@ -9,6 +9,49 @@
     }
 
     const dataPromise = fetch(`${base}/photos.json`).then(res => res.json());
+
+    //d↓
+    function getColWidth(column: string, options: any = {}): number {
+        let width = 0;
+        if (column.length > 0) {
+            const colChars = column.split('');
+            for (let i = 0; i < colChars.length; i++) {
+            width += colChars[i].charCodeAt(0) * (i + 1);
+            }
+        } else {
+            if (options && options.hasOwnProperty('defaultWidth')) {
+            width = options.defaultWidth;
+            } else {
+            width = Math.random() * 100;
+            }
+        }
+
+        if (options && options.hasOwnProperty('round')) {
+            if (options.round === 'up') {
+            width = Math.ceil(width);
+            } else if (options.round === 'down') {
+            width = Math.floor(width);
+            }
+        }
+
+        if (options && options.hasOwnProperty('unit')) {
+            if (options.unit === 'px') {
+            return width;
+            } else if (options.unit === 'em') {
+            return width / 16;
+            } else if (options.unit === 'rem') {
+            return width / 8;
+            } else {
+            throw new Error('Invalid unit specified.');
+            }
+        }
+
+        return width;
+        }
+
+        // Usage:
+        const colWidth = getColWidth('ABC', { round: 'up', unit: 'em' });
+        console.log('Column Width:', colWidth, 'em');
 </script>
 
 <Header />
