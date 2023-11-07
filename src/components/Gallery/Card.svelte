2f044423f9d31fb9ff058d8258b616afd36d1f10
0324bence <bence20050324@gmail.com>
2023-11-07 16:40:16 +0100

    Modify card to meet needs

--- src/components/Gallery/Card.svelte
+++ src/components/Gallery/Card.svelte
@@ -1,39 +1,34 @@
-
 <script lang="ts">
-    import { base } from "$app/paths"
-    export let title:string;
-    export let price:string;
-    export let imageID:string;
+    import { base } from "$app/paths";
+    export let title: string;
+    export let price: string;
+    export let imageID: string;
+    export let image: string;
 </script>
 
-
 <a href="gallery/{imageID}" class="card-link">
-    <div class="card" style="background-image: url('{base}/images/photos/{imageID}.webp');">
+    <div class="card" style="background-image: url('{base}/images/photos/{image}');">
         <div class="card-belt">
             <div class="card-title">
-                <p> {title}</p>
+                <p>{title}</p>
             </div>
             <div class="card-price">
                 <p>{price}</p>
             </div>
-            
-
         </div>
     </div>
 </a>
 
 <style lang="scss">
-
-    @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&family=Share+Tech&display=swap');
+    @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&family=Share+Tech&display=swap");
 
     * {
-        font-family:'Rubik', 'Arial Narrow', Arial, sans-serif;
+        font-family: "Rubik", "Arial Narrow", Arial, sans-serif;
         text-decoration: none !important;
         color: black;
     }
 
-    .card{
-
+    .card {
         min-width: 18em;
         max-width: 22em;
         aspect-ratio: 16/9;
@@ -43,7 +38,7 @@
         border-radius: 6px;
         border: solid 1px black;
     }
-    .card-belt{
+    .card-belt {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
@@ -51,10 +46,10 @@
         width: 100%;
         max-height: 2.9em;
         height: 23%;
-        background-color: rgba($color: #fff, $alpha: 0.70);
+        background-color: rgba($color: #fff, $alpha: 0.7);
         border-radius: 0px 0px 6px 6px;
     }
-    .card-title{
+    .card-title {
         flex: 1.4;
         display: flex;
         padding-left: 9px;
@@ -64,7 +59,7 @@
         justify-content: start;
         font-size: 1.1em;
     }
-    .card-price{
+    .card-price {
         flex: 0.6;
         display: flex;
         align-items: center;
@@ -73,11 +68,5 @@
         padding-right: 9px;
         font-weight: 600;
         font-size: 0.9em;
-
     }
-
-    
-
-
-
 </style>
